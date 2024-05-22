b3a6ba245fa5fd9e4547c9390cc4ed7f785d3f7f
Burak <burakduran12491@gmail.com>
2024-05-22 15:11:24 +0300

    Migrations operasyonu tamamlandı

---
+++ Core/ETicaretAPI.Domain/Entities/Customer.cs
@@ -0,0 +1,15 @@
+﻿using ETicaretAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Domain.Entities
+{
+    public class Customer : BaseEntitiy
+    { // Bir orderin birden fazla customeri olamaz
+        public string Name { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
---
+++ Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using ETicaretAPI.Persistence.Contexts;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services) //IoC konteynerimin arayuzune bir extension fonksiyon sagliyor.Bu fonk uzerinden ben direkt Ioc konteynerime datalarimi gonderebiliyorum.
+        {
+            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString)); //connection string kod icine manuel verilmemeli
+        }
+
+
+    }
+}
